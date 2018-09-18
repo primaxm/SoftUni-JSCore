e3fd39d5aa466fb97be4f937c6b9a6fc804e108f
Primaxm <primaxm@gmail.com>
2018-09-18 20:35:30 +0300

    more exersices

--- JS Advanced/jQuery Library/wiki-parser.js
+++ JS Advanced/jQuery Library/wiki-parser.js
@@ -1,25 +1,45 @@
 function wikiParser(selector) {
     let text = $(selector).text();
-    let treeEqualsRegEx = /===(.*?)===/g;
-    let matches = text.match(treeEqualsRegEx);
+    let treeEqualsRegEx = /===(.*?)===/;
+    let twoEqualsRegEx = /==(.*?)==/;
+    let oneEqualsRegEx = /=(.*?)=/;
+    let threeSingleQuotesRegEx = /'''(.*?)'''/;
+    let twoSingleQuotesRegEx = /''(.*?)''/;
+    let twoBracketsRegEx = /\[\[([^\[\]]*?)\]\]/;
 
-    console.log(matches);
-    //let twoSingleQuotesRegEx = /(?<=(===)).+?(?=(===))/g;
-    //console.log(text.match(twoSingleQuotesRegEx));
+    text = replace(text, treeEqualsRegEx, "h3");
+    text = replace(text, twoEqualsRegEx, "h2");
+    text = replace(text, oneEqualsRegEx, "h1");
+    text = replace(text, threeSingleQuotesRegEx, "b");
+    text = replace(text, twoSingleQuotesRegEx, "i");
+    text = brackesReplace(text, twoBracketsRegEx);
 
-    text = text.replace(twoSingleQuotesRegEx, '<h3>$&</h3>')
-    //text.replace(twoSingleQuotesRegEx, 'kuku')
-    console.log(text);
 
-    function replace (regEx, text) {
+    function replace (text, regEx, tag) {
+        let matches = text.match(regEx);
 
+        while(matches) {
+
+            text = text.replace(matches[0], `<${tag}>${matches[1]}</${tag}>`);
+            matches = text.match(regEx);
+        }
+        return text;
     }
 
-}
-/*
+    function brackesReplace (text, regEx) {
+        let match = text.match(regEx);
 
-''text'' becomes <i>text</i> (two single quotes)
-'''text''' becomes <b>text</b> (three single quotes)
- =text=, ==text== and ===text=== become <h1>text</h1>, <h2>text</h2> and <h3>text</h3>
- [[link]] becomes an anchor to /wiki/link with the same text, <a href="/wiki/link">link</a>
- [[link|Text]] becomes an anchor to /wiki/link with the argument after the pipe as text, <a href="/wiki/link">Text</a>*/
+        while(match) {
+            if (match[1].indexOf("|") > -1) {
+                let splited = match[1].split('|');
+                text = text.replace(match[0], `<a href="/wiki/${splited[0]}">${splited[1]}</a>`);
+            } else {
+                text = text.replace(match[0], `<a href="/wiki/${match[1]}">${match[1]}</a>`);
+            }
+            match = text.match(regEx);
+        }
+        return text;
+    }
+
+    $(selector).html(text)
+}
